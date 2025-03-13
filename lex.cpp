665c72585f8f6fba2511faac8f9335a451320c9f
birodavidjanos <biro.david.janos@gmail.com>
2025-03-13 14:01:47 +0100

    Végső megoldás

--- lex.cpp
+++ lex.cpp
@@ -1,77 +1,94 @@
+//Lexikális fordító
+
+
+
+//példa input: 2003ISTRAB2003>=ARVAI{*DAVID**}<>(**CSABA*)128TAMAS$
+//példa output:<konstans><azonsító><reláció> <azonosító> <relácó>...<konstans><EOF>
+
 #include <iostream>
-#include <regex>
+#include <string.h> 
 #include <string>
-#include <vector>
 
-// Token típusok
-enum class TokenType {
-    KONSTANS,
-    AZONOSITO,
-    OPERÁTOR,
-    RELÁCIÓS_JEL,
-    FAJL_VEGE
-};
+using namespace std;
 
-// Token struktúra
-struct Token {
-    TokenType type;
-    std::string value;
-};
+int main()
+{
+    setlocale(LC_ALL, "hu");
 
-// Tokenizáló függvény
-std::vector<Token> lexikalalisElemzo(const std::string& bemenet) {
-    std::vector<Token> tokenek;
-    std::regex tokenRegEx(R"((\d+|[A-ZÁÉÍÓÖŐÚÜŰ]+|[=><!*{}()$]))"); // Regular expression a tokenekhez
-    auto words_begin = std::sregex_iterator(bemenet.begin(), bemenet.end(), tokenRegEx);
-    auto words_end = std::sregex_iterator();
+    string userinput= "";
+    string output="";
 
-    for (auto it = words_begin; it != words_end; ++it) {
-        std::string token = it->str();
+    std::cout << "\nInput:\n~~~~~~~~~\n";
 
-        // Azonosítók és konstansok kezelése
-        if (std::isdigit(token[0])) {
-            tokenek.push_back({TokenType::KONSTANS, token});
-        } else if (token == "{" || token == "}" || token == "*" || token == "**" || token == "(" || token == ")" || token == "<>" || token == "$") {
-            tokenek.push_back({TokenType::OPERÁTOR, token});
-        } else {
-            tokenek.push_back({TokenType::AZONOSITO, token});
+    getline(cin, userinput);
+    int length =userinput.length();
+    //cout << "Input length: "<<length;
+
+
+   for (int i=0; i < length; ++i) {
+      
+       if (isdigit(userinput[i])) {
+           while (isdigit(userinput[i])) {
+               ++i;
+           }
+           output += "<konstans>";
+       }
+       if (isalpha(userinput[i])) {
+           while (isalpha(userinput[i])) {
+               ++i;
+           }
+           output += "<azonosító>";
+       }
+       if (userinput[i]==':' && userinput[i+1] == '=') {
+           i=i+2;
+           output += "<reláció>";
+       }
+       
+       //--------Ha { karaktert talál végigmegy a maradék karakter sorozaton a } karakter elsõ elõfordulását keresve
+       if (userinput[i] == '{') {
+           for (int j=i; j < length; ++j) {
+               if (userinput[j] == '}') {
+                   i = j;
+                   output += "<{}komment>";
+                   break;
+               }
+           }
+       }
+
+       //--------Ha ( és * követi egymást végigmegy a maradék karakter sorozaton * ) karakterek elsõ elõfordulását keresve
+       if (userinput[i] == '(' && userinput[i+1]=='*') {
+           for (int j = i+2; j < length; ++j) {
+               if (userinput[j] == '*' && userinput[j+1]==')') {
+                   i = j+1;
+                   output += "<(**)komment>";
+                   break;
+               }
+           }
         }
-    }
+       
+       if (userinput[i] == '<' && userinput[i + 1] == '>') {
+           output += "<reláció>";
 
-    // EOF token hozzáadása
-    tokenek.push_back({TokenType::FAJL_VEGE, "$"});
+       }
 
-    return tokenek;
-}
+       if (userinput[i] == '<' && userinput[i + 1] == '=') {
+           output += "<reláció>";
 
-// Tokenek kiírása
-void kiirTokenek(const std::vector<Token>& tokenek) {
-    for (const auto& token : tokenek) {
-        switch (token.type) {
-            case TokenType::KONSTANS:
-                std::cout << "konstans: " << token.value << std::endl;
-                break;
-            case TokenType::AZONOSITO:
-                std::cout << "azonosito: " << token.value << std::endl;
-                break;
-            case TokenType::OPERÁTOR:
-                std::cout << "operátor vagy relációsjel: " << token.value << std::endl;
-                break;
-            case TokenType::FAJL_VEGE:
-                std::cout << "eof: " << token.value << std::endl;
-                break;
-        }
-    }
-}
+       }
 
-int main() {
-    std::string bemenet = "2003ISTRÁB2003>=ÁRVAI{*DÁVID**}<>(**CSABA*)128TAMÁS$";
-    
-    // Tokenizálás
-    std::vector<Token> tokenek = lexikalalisElemzo(bemenet);
+       if (userinput[i] == '>' && userinput[i + 1] == '=') {
+           output += "<reláció>";
 
-    // Tokenek kiírása
-    kiirTokenek(tokenek);
+       }
 
-    return 0;
+       
+
+       //-----Amikor $ karaktert talál befejezi az elemzést
+       if (userinput[i] == '$') {
+           output += "<EOF>";
+           break; 
+       }
+   }
+
+   cout <<"\nOutput:\n~~~~~~~~~\n"<< output << "\n \n \n";
 }
